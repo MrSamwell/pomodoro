10c8d70f434307df8c6bf1402c99b7872376fe84
MrSamwell <samuelbs98@gmail.com>
2020-05-10 12:58:46 -0300

    Some progress

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ new Vue({
     el: '#app',
     data: {
         display: true,
+        isPlaying: false,
+        st_worktime: 25,
+        st_shortbreak: 5,
+        st_longbreak: 25,
         worktime: 25,
         shortbreak: 5,
         longbreak: 25
@@ -10,6 +14,21 @@ new Vue({
         changeDisplay: function(){
             this.display = !this.display;
         },
+        applyChanges: function(){
+           this.changeDisplay();
+           this.worktime = this.st_worktime;
+           this.shortbreak = this.st_shortbreak;
+           this.longbreak = this.st_longbreak;
+        //    console.log(`Worktime: ${this.worktime }, Shortbreak: ${this.shortbreak}, LongBreak ${this.st_longbreak}`);
+        },
+        cancelChanges: function(){
+            this.changeDisplay();
+            this.st_worktime = this.worktime;
+            this.st_shortbreak = this.shortbreak;
+            this.st_longbreak = this.longbreak;
+
+            // console.log(`Worktime: ${this.worktime }, Shortbreak: ${this.shortbreak}, LongBreak ${this.st_longbreak}`);
+         }
         
     }
 })
