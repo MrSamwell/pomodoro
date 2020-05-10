c9abaf978414cd16f1c3401eaf06bdbdb540b287
MrSamwell <samuelbs98@gmail.com>
2020-05-10 16:40:22 -0300

    Some progress

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ new Vue({
         st_longbreak: 25,
         worktime: 25,
         shortbreak: 5,
-        longbreak: 25
+        longbreak: 25,
+        displayTime: false
     },
     methods:{
         changeDisplay: function(){
@@ -19,6 +20,7 @@ new Vue({
            this.worktime = this.st_worktime;
            this.shortbreak = this.st_shortbreak;
            this.longbreak = this.st_longbreak;
+           this.displayTime = this.worktime * 60;
         //    console.log(`Worktime: ${this.worktime }, Shortbreak: ${this.shortbreak}, LongBreak ${this.st_longbreak}`);
         },
         cancelChanges: function(){
@@ -28,7 +30,36 @@ new Vue({
             this.st_longbreak = this.longbreak;
 
             // console.log(`Worktime: ${this.worktime }, Shortbreak: ${this.shortbreak}, LongBreak ${this.st_longbreak}`);
-         }
-        
+         },
+         formatDisplay: function(sec){
+             if(this.displayTime == ''){
+            this.displayTime = this.worktime * 60;}
+
+            var minutes = Math.floor(sec/60);
+            var seconds = sec%60;
+
+            if(minutes >= 0 && minutes<=9){
+                minutes = '0' + minutes
+            }
+            if(seconds >= 0 && seconds<=9){
+                seconds = '0' + seconds
+            }
+            
+            return `${minutes}:${seconds}`
+         },
+         btnPlayClicked: function(){
+             this.isPlaying = !this.isPlaying;
+             this.startTimer();
+         },
+         startTimer: function() {
+             while(this.isPlaying){
+                 setTimeout(this.startTimer, 1000);
+                 this.displayTime -= 1;  
+             }
+         },
+        mounted: function(){
+           
+           
+        }
     }
 })
